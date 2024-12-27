c3d1a49ba63d6fdb20ae4c23552ec21aea983612
Patrick Knobbout <patrick@zonarr.nl>
2024-12-27 23:50:15 +0100

    Day 3

--- 2024/3/part1.ps1
+++ 2024/3/part1.ps1
@@ -1,2 +1,20 @@
+Clear-Host
 $inputData = Get-Content -Path "$PSScriptRoot\input.txt"
 
+$parts = $inputData -split "mul\("
+
+$total = 0
+foreach ($p in $parts) {
+    if ($p -match "\)") {
+        $calculation = ($p -split "\)")[0]
+        $calc = $calculation -split ","
+        try {
+            $add = ([int]$calc[0] * [int]$calc[1])
+            $total += $add
+        } catch { 
+            # $calculation
+        }
+    }
+}
+
+$total
--- 2024/3/part2.ps1
+++ 2024/3/part2.ps1
@@ -1,1 +1,32 @@
+Clear-Host
 $inputData = Get-Content -Path "$PSScriptRoot\input.txt"
+
+$parts = $inputData -split "mul\("
+
+$total = 0
+
+$enabled = $true
+for ($i=0; $i -lt $parts.Count; $i++) {
+    if ($enabled) {
+        # $parts[$i]
+        if ($parts[$i] -match "\)") {
+            $calculation = ($parts[$i] -split "\)")[0]
+            $calc = $calculation -split ","
+            try {
+                $add = ([int]$calc[0] * [int]$calc[1])
+                $total += $add
+            } catch { 
+                # $calculation
+            }
+        }
+    }
+    if ($parts[$i] -match "don't\(\)") {
+        # Write-Host $parts[$i] -ForegroundColor Red
+        $enabled = $false
+    } elseif ($parts[$i] -match "do\(\)") {
+        # Write-Host $parts[$i] -ForegroundColor Green
+        $enabled = $true
+    }
+}
+
+$total
